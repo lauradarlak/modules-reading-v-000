1bf3adc0c82b9449c3347e0f0c260d1b2336976a
Laura Darlak <laura@polyporedesign.com>
2018-09-07 16:34:12 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,7 +4,7 @@ require_relative './fancy_dance.rb'
 
 class Dancer
   include FancyDance::InstanceMethods
-  extend FanceyDance::ClassMethods
+  extend FancyDance::ClassMethods
   
   attr_accessor :name
  
